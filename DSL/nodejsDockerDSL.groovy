73dc457aa409ca02e831469a86a679503d48dab8
Hossmell Velasco <velasco2288@gmail.com>
2023-10-19 09:35:04 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,20 +1,20 @@
-job('Aplicacion Node.js Docker DSL') {
+job('AplicacionNodeDockerDSLDocker') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/hossvel/nodejsapp.git', 'master') { node ->
+             node / gitConfigName('hhvelascoa')
+             node / gitConfigEmail('hhvelascoa@gmail.com')
         }
     }
     triggers {
         scm('H/7 * * * *')
     }
     wrappers {
-        nodejs('nodejs')
+        nodejs('nodejenkins')
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('hhvelascoa/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
